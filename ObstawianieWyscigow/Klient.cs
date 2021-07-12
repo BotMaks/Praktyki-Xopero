2d65feb56b7e36c0b118cc77798284cd626fa2c5
BotMaks <maksymilian.adamajtis@gmail.com>
2021-07-12 11:03:40 +0200

    .

---
+++ ObstawianieWyscigow/Klient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObstawianieWyscigow
+{
+    class Klient
+    {
+        public string imie;
+        public int konto;
+        public RadioButton mojPrzycisk;
+        public Label mojTekst;
+        public Zaklad mojZaklad = new Zaklad();
+
+        public Klient() { }
+
+        public Klient(string imie, int konto, RadioButton mojPrzycisk, Label mojTekst) {
+            this.imie = imie;
+            this.konto = konto;
+            this.mojPrzycisk = mojPrzycisk;
+            this.mojTekst = mojTekst;
+        }
+        public void aktualizujDane() {
+            mojPrzycisk.Text = string.Format("{0} ma {1} zł", imie, konto);
+            /*if (mojZaklad) {
+                mojTekst.Text = string.Format("{0}");
+            }
+            else
+            {
+                mojTekst.Text = string.Format("");
+            }*/
+        }
+        public void wyczyscZaklad() { }
+
+        /*public bool postawZaklad(int ilosc, int zawodnik) {
+            Zaklad mojZaklad = new Zaklad(ilosc, zawodnik, this);
+            if () { }
+            return ;
+        }*/
+        public void pobierzZaklad() { }
+    }
+}
---
+++ ObstawianieWyscigow/Zaklad.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObstawianieWyscigow
+{
+    class Zaklad
+    {
+        public int ilosc = 0;
+        public int nrZawodnika;
+        public Klient klientZakladu;
+
+        public string opisZakladu(){
+            if (ilosc == 0) { return string.Format("{0} nie zawarł zakładu", klientZakladu.imie); } 
+            else {
+                return string.Format("{0} postawił {1} zł na zawodnika numer {2}", klientZakladu.imie, ilosc, nrZawodnika);
+            }
+        }
+        public int wyplata(int zwyciezca) {
+            if (zwyciezca == nrZawodnika) {
+                return ilosc;
+            }
+            else
+            {
+                return ilosc * (-1);
+            }
+        }
+        public Zaklad() { }
+
+        public Zaklad(int ilosc,int nrZawodnika,Klient klientZakladu) {
+            this.ilosc = ilosc;
+            this.nrZawodnika = nrZawodnika;
+            this.klientZakladu = klientZakladu;
+        }
+    }
+}
