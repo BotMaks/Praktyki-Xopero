fd1016148fd3dd174d567cd292671aa4cbe52982
BotMaks <maksymilian.adamajtis@gmail.com>
2021-07-14 10:21:56 +0200

    .

---
+++ Program dla Krystyny/Impreza.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Program_dla_Krystyny
+{
+    class Impreza
+    {
+        private int ilośćOsób;
+        private double kosztJedzenia = 30;
+        private bool dekoracje;
+        private bool rabat;
+        private double całkowityKoszt;
+        public Impreza() { }
+
+        public void ustawIlośćOsób(int ilośćOsób)
+        {
+            this.ilośćOsób = ilośćOsób;
+        }
+
+        public void ustawKosztJedzenia(bool opcjaZdrowa) {
+            if (opcjaZdrowa)
+            {
+                rabat = true;
+                kosztJedzenia = 30;
+            }
+            else
+            {
+                rabat = false;
+                kosztJedzenia = 45;
+            }
+        }
+
+        public void ustawDekoracje(bool dekoracje) {
+            if (dekoracje) {
+                this.dekoracje = true;
+            }
+            else{
+                this.dekoracje = false;
+            }
+        }
+        public string obliczCałkowityKoszt() {
+
+            if (dekoracje == true) {
+                całkowityKoszt = 50 + (ilośćOsób * kosztJedzenia) + (ilośćOsób * 15);
+            }
+            else
+            {
+                całkowityKoszt = 30 + (ilośćOsób * kosztJedzenia) + (ilośćOsób * 7.5);
+            }
+                if (rabat)
+            {
+                całkowityKoszt *= 0.95;
+            }
+            return Math.Round(całkowityKoszt,2).ToString();
+        }
+    }
+}
---
+++ Program dla Krystyny/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Program_dla_Krystyny
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Impreza impreza = new Impreza();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void kosztImprezy_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void opcjaZdrowa_Click(object sender, RoutedEventArgs e)
+        {
+            impreza.ustawKosztJedzenia(opcjaZdrowa.IsChecked.Value);
+            kosztImprezy.Text = impreza.obliczCałkowityKoszt();
+        }
+
+        private void ilośćOsób_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                impreza.ustawIlośćOsób(Convert.ToInt32(ilośćOsób.Text));
+                kosztImprezy.Text = impreza.obliczCałkowityKoszt();
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Wprowadź poprawne dane!");
+            }
+        }
+
+        private void dekoracje_Click(object sender, RoutedEventArgs e)
+        {
+            impreza.ustawDekoracje(dekoracje.IsChecked.Value);
+            kosztImprezy.Text = impreza.obliczCałkowityKoszt();
+        }
+    }
+}
