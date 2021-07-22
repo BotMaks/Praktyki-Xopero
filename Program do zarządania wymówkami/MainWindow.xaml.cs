7836643efc00aa47b3ac7cc6c22a5cb3b5ea1d4b
BotMaks <maksymilian.adamajtis@gmail.com>
2021-07-22 15:49:23 +0200

    implementacja logiki dla przycisku folder

--- Program do zarządania wymówkami/MainWindow.xaml.cs
+++ Program do zarządania wymówkami/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+//using Microsoft.Win32;
+//using System.Windows.Forms;
 
 namespace Program_do_zarządania_wymówkami
 {
@@ -27,7 +30,30 @@ namespace Program_do_zarządania_wymówkami
 
         private void folderButton_Click(object sender, RoutedEventArgs e)
         {
+            using(System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult wynik = fbd.ShowDialog();
+                if (wynik == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) 
+                {
+                    string ścieżka = fbd.SelectedPath;
+                    string[] pliki = Directory.GetFiles(fbd.SelectedPath);
 
+                    
+                    MessageBox.Show(string.Format("Podana ścieżka: {0}\nZnalezione pliki: {1}", ścieżka, pliki.Length));
+                }
+            }
+
+            MessageBox.Show("Podano niepoprawne dane!", "Błąd");
+            /*OpenFileDialog wybierzFolder = new OpenFileDialog();
+            string ścieżka = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            wybierzFolder.InitialDirectory = @ścieżka;
+            wybierzFolder;
+            Nullable<bool> wynik = wybierzFolder.ShowDialog();
+            if (wynik == true)
+            {
+                string nazwaPliku = wybierzFolder.FileName;
+            }
+            else { MessageBox.Show("Nie wybrałeś folderu"); }*/
         }
 
         private void zapiszButton_Click(object sender, RoutedEventArgs e)
---
+++ Program do zarządania wymówkami/Wymówka.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_do_zarządania_wymówkami
+{
+    class Wymówka
+    {
+        private string nazwa;
+        private string ścieżka;
+
+        public Wymówka() { }
+    
+
+    }
+}
