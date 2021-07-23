f2714f9807ab7ed4cc233213a5dd417198b79afd
BotMaks <maksymilian.adamajtis@gmail.com>
2021-07-23 12:11:49 +0200

    Poprawienie przycisku folder i dodanie logiki do przycisku otwórz i wybierz losową

--- Program do zarządania wymówkami/MainWindow.xaml.cs
+++ Program do zarządania wymówkami/MainWindow.xaml.cs
@@ -13,8 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
-//using Microsoft.Win32;
-//using System.Windows.Forms;
+
 
 namespace Program_do_zarządania_wymówkami
 {
@@ -23,6 +22,11 @@ namespace Program_do_zarządania_wymówkami
     /// </summary>
     public partial class MainWindow : Window
     {
+        string ścieżka;
+        string[] pliki;
+        Wymówka aktualnaWymówka = new Wymówka();
+        Random rand = new Random();
+        private bool zmianaFormularza = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,43 +36,74 @@ namespace Program_do_zarządania_wymówkami
         {
             using(System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog())
             {
+                fbd.Description = "Wybierz folder z wymówkami";
                 System.Windows.Forms.DialogResult wynik = fbd.ShowDialog();
                 if (wynik == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) 
                 {
-                    string ścieżka = fbd.SelectedPath;
-                    string[] pliki = Directory.GetFiles(fbd.SelectedPath);
+                    ścieżka = fbd.SelectedPath;
+                    pliki = Directory.GetFiles(fbd.SelectedPath);
 
-                    
-                    MessageBox.Show(string.Format("Podana ścieżka: {0}\nZnalezione pliki: {1}", ścieżka, pliki.Length));
+                    aktualnaWymówka = new Wymówka(ścieżka);
+
+                    zapiszButton.IsEnabled = true;
+                    otwórzButton.IsEnabled = true;
+                    losowaWymówkaButton.IsEnabled = true;
+                    wymówkaTextBox.IsEnabled = true;
+                    wynikiTextBox.IsEnabled = true;
+                    ostatnioUżyteTextBox.IsEnabled = true;
+                    return;
                 }
             }
 
-            MessageBox.Show("Podano niepoprawne dane!", "Błąd");
-            /*OpenFileDialog wybierzFolder = new OpenFileDialog();
-            string ścieżka = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            wybierzFolder.InitialDirectory = @ścieżka;
-            wybierzFolder;
-            Nullable<bool> wynik = wybierzFolder.ShowDialog();
-            if (wynik == true)
+            MessageBox.Show("Podano niepoprawne dane!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+        private void otwórzButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!sprawdźZmiany())
             {
-                string nazwaPliku = wybierzFolder.FileName;
+                using (System.Windows.Forms.OpenFileDialog otwórz = new System.Windows.Forms.OpenFileDialog())
+                {
+                    otwórz.InitialDirectory = ścieżka;
+                    otwórz.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+                    otwórz.Title = "Wybierz plik z wymówką";
+                    System.Windows.Forms.DialogResult wynik = otwórz.ShowDialog();
+                    if (wynik == System.Windows.Forms.DialogResult.OK)
+                    {
+                        aktualnaWymówka.otwórzWymówkę(otwórz.FileName, wymówkaTextBox, wynikiTextBox, ostatnioUżyteTextBox);
+                    }
+                }
             }
-            else { MessageBox.Show("Nie wybrałeś folderu"); }*/
+        }
+        private void losowaWymówkaButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!sprawdźZmiany())
+            {
+                aktualnaWymówka = new Wymówka(rand, ścieżka);
+                aktualnaWymówka.otwórzWymówkę(aktualnaWymówka.nazwa, wymówkaTextBox, wynikiTextBox, ostatnioUżyteTextBox);
+            }
         }
 
         private void zapiszButton_Click(object sender, RoutedEventArgs e)
         {
+            if(String.IsNullOrEmpty(wymówkaTextBox.Text) || String.IsNullOrEmpty(wynikiTextBox.Text))
+            {
+                MessageBox.Show("Podaj wymówkę i rezultat!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
 
         }
 
-        private void otwórzButton_Click(object sender, RoutedEventArgs e)
+        private bool sprawdźZmiany()
         {
-
-        }
-
-        private void losowaWymówkaButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            if (aktualnaWymówka.nazwa != wymówkaTextBox.Text || aktualnaWymówka.wynik != wynikiTextBox.Text || aktualnaWymówka.ostatnioUżyta != ostatnioUżyteTextBox.Text)
+            {
+                MessageBoxResult odpowiedź = MessageBox.Show("Bierząca wymówka nie zostałą zapisana!\nCzy kontynuować?", "Ostrzeżenie", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (odpowiedź == MessageBoxResult.No)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- Program do zarządania wymówkami/Wymówka.cs
+++ Program do zarządania wymówkami/Wymówka.cs
@@ -3,16 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace Program_do_zarządania_wymówkami
 {
     class Wymówka
     {
-        private string nazwa;
-        private string ścieżka;
+        public string nazwa { get; set; }
+        public string wynik { get; set; }
+        public string ścieżka { get; set; }
+        public string ostatnioUżyta { get; set; }
 
-        public Wymówka() { }
+        public Wymówka() { ścieżka = ""; }
     
+        public Wymówka(string ścieżka) 
+        {
+            this.ścieżka = ścieżka;
+            nazwa = "";
+            wynik = "";
+            ostatnioUżyta = "";
+        }
 
+        public Wymówka(Random rand, string ścieżka) 
+        {
+            this.ścieżka = ścieżka;
+            string[] pliki = Directory.GetFiles(ścieżka, "*.txt");
+            this.nazwa = pliki[rand.Next(pliki.Length)];
+            
+        }
+
+        public void zapiszWymówkę(string nazwa, TextBox wymówka, TextBox opis, TextBox data) 
+        {
+            using (StreamWriter zapisz = new StreamWriter(nazwa)) 
+            {
+                zapisz.WriteLine(wymówka.Text);
+                zapisz.WriteLine(opis.Text);
+                zapisz.WriteLine(data.Text);
+            }
+            this.nazwa = wymówka.Text;
+            this.wynik = opis.Text;
+            this.ostatnioUżyta = data.Text;
+                
+        }
+
+        public void otwórzWymówkę(string nazwa, TextBox wymówka, TextBox opis, TextBox data) 
+        {
+            using (StreamReader czytaj = new StreamReader(nazwa)) 
+            {
+                while (!czytaj.EndOfStream) {
+                    wymówka.Text = czytaj.ReadLine();
+                    opis.Text = czytaj.ReadLine();
+                    data.Text = czytaj.ReadLine();
+                }
+            }
+            this.nazwa = wymówka.Text;
+            this.wynik = opis.Text;
+            this.ostatnioUżyta = data.Text;
+        }
     }
 }
